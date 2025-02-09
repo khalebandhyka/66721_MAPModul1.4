9fb0019df36fc267ea81777e655010bc6baac26d
Khaleb Andhyka Aprijadi <khaleb.andhyka@gmail.com>
2025-02-09 23:24:30 +0700

    ViewModels dan LiveData

--- app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
+++ app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailFragment.kt
@@ -116,7 +116,7 @@ class PlantDetailFragment : Fragment() {
             composeView.setContent {
                 // You're in Compose world!
                 MaterialTheme {
-                    PlantDetailDescription()
+                    PlantDetailDescription(plantDetailViewModel)
                 }
             }
             }
